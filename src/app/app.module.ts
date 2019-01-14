1bf9a927ad73baf5d21af718f16199dfdf311acd
Andreas Hallberg <andreas.hallberg@infor.com>
2019-01-14 13:53:00 +0100

    set up ids-enterprise-ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { SohoComponentsModule } from 'ids-enterprise-ng';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SohoComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
