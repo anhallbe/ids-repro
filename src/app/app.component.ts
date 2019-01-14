d35cb82a836690f7dd7e9c0bb20780a219a82df9
Andreas Hallberg <andreas.hallberg@infor.com>
2019-01-14 14:24:32 +0100

    repro accordion click listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable, of, Subject } from 'rxjs';
+import { delay, scan } from 'rxjs/operators';
+import { SohoToastService } from 'ids-enterprise-ng';
+
+type Link = string;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ids-repro';
+  private logSubject = new Subject<string>();
+
+  title = 'Links without delay';
+  links$: Observable<Link[]> = of([
+    'Non-delayed Link 1',
+    'Non-delayed Link 2',
+    'Non-delayed Link 3'
+  ]);
+
+  // Uncomment below to show working click listeners
+  // title = 'Links with delay';
+  // links$: Observable<Link[]> = of([
+  //   'Delayed Link 1',
+  //   'Delayed Link 2',
+  //   'Delayed Link 3'
+  // ]).pipe(delay(100));
+
+  logs$: Observable<string[]> = this.logSubject.pipe(
+    scan<string, string[]>((entries, entry) => [...entries, entry], []),
+  );
+
+  constructor(private toastService: SohoToastService) {}
+
+  openLink(link: Link) {
+    const logEntry = `${link} clicked!`;
+    console.log(logEntry);
+    this.logSubject.next(logEntry);
+    this.toastService.show({
+      title: 'It works!',
+      message: logEntry,
+      timeout: 1000
+    });
+  }
 }
