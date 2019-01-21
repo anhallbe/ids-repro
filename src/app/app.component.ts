85c89bcee6175707207b78bc08056150594512af
Andreas Hallberg <andreas.hallberg@infor.com>
2019-01-21 13:07:59 +0100

    card-in-widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable, of, Subject } from 'rxjs';
-import { delay, scan } from 'rxjs/operators';
-import { SohoToastService } from 'ids-enterprise-ng';
-
-type Link = string;
 
 @Component({
   selector: 'app-root',
@@ -11,37 +6,11 @@ type Link = string;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private logSubject = new Subject<string>();
-
-  title = 'Links without delay';
-  links$: Observable<Link[]> = of([
-    'Non-delayed Link 1',
-    'Non-delayed Link 2',
-    'Non-delayed Link 3'
-  ]);
-
-  // Uncomment below to show working click listeners
-  // title = 'Links with delay';
-  // links$: Observable<Link[]> = of([
-  //   'Delayed Link 1',
-  //   'Delayed Link 2',
-  //   'Delayed Link 3'
-  // ]).pipe(delay(100));
-
-  logs$: Observable<string[]> = this.logSubject.pipe(
-    scan<string, string[]>((entries, entry) => [...entries, entry], []),
-  );
-
-  constructor(private toastService: SohoToastService) {}
-
-  openLink(link: Link) {
-    const logEntry = `${link} clicked!`;
-    console.log(logEntry);
-    this.logSubject.next(logEntry);
-    this.toastService.show({
-      title: 'It works!',
-      message: logEntry,
-      timeout: 1000
-    });
-  }
+  list = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
 }
