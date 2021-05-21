bddf85125730f41f43177b66fc9ee246ce64aae2
Andreas Hallberg <andreas.hallberg@infor.com>
2021-05-21 09:18:53 +0200

    Angular 10 to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     RouterModule,
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
   ]
 })
 export class AppRoutingModule { }
