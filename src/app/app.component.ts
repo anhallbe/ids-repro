cf6e2329a10bf12de24eab1a3e3b346e921667f6
Andreas Hallberg <andreas.hallberg@infor.com>
2022-01-21 15:01:32 +0100

    add version to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  idsVersion: string;
+  idsNgVersion: string;
 
+  ngOnInit() {
+    const htmlElement = document.querySelector('html');
+    this.idsVersion = htmlElement.getAttribute('data-sohoxi-version');
+    this.idsNgVersion = htmlElement.getAttribute('data-ids-enterprise-ng-version');
+    console.log(this.idsVersion, this.idsNgVersion)
+  }
 }
